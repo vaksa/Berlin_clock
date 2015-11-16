ae4ae21915b584cee4fb2796736ace4a5b1b204b
IIpu3pak <IIpu3pak7@gmail.com>
2015-11-16 20:41:16 +0200

    Develop data base model

--- model/src/main/java/com/becomejavasenior/User.java
+++ model/src/main/java/com/becomejavasenior/User.java
@@ -1,42 +1,43 @@
 package com.becomejavasenior;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.*;
 
-public class User {
+/**
+ * @author Orlov Vladislav on 15.11.2015.
+ */
+public class User implements Serializable{
 
     private String login;
     private String password;
-    private Roles role;
+    private Set<Role> roles;
     private String firstName;
     private String lastName;
     private String email;
+    private String whiteListIP;
 
-    public enum Roles {ADMINISTRATOR, SUBSCRIBER}
 
     public static Map<String, User> getValidUsers(){
 
-        Map<String, User> validUsers = new HashMap<>();
+       /* Map<String, User> validUsers = new HashMap<>();
         validUsers.put("Administrator", new User("Administrator", "111", Roles.ADMINISTRATOR));
         validUsers.put("Guest1", new User("Guest", "222", Roles.SUBSCRIBER));
         validUsers.put("Guest2", new User("Guest", "333", Roles.SUBSCRIBER));
-        validUsers.put("Guest3", new User("Guest", "444", Roles.SUBSCRIBER));
+        validUsers.put("Guest3", new User("Guest", "444", Roles.SUBSCRIBER));*/
 
-        return validUsers;
+        return null;
     }
 
     public static List<User> getValidUsersList(){
 
-        List< User> validUsers = new ArrayList<>();
+        /*List< User> validUsers = new ArrayList<>();
         validUsers.add(new User("Administrator", "111", Roles.ADMINISTRATOR, "Andy", "Admynman", "anad@gmail.com"));
         validUsers.add(new User("Guest1", "222", Roles.SUBSCRIBER, "Guest", "Account", "1"));
         validUsers.add(new User("Guest2", "333", Roles.SUBSCRIBER, "Guest", "Account", "2"));
-        validUsers.add(new User("Guest3", "444", Roles.SUBSCRIBER, "Guest", "Account", "3"));
+        validUsers.add(new User("Guest3", "444", Roles.SUBSCRIBER, "Guest", "Account", "3"));*/
 
-        return validUsers;
+        return null;
     }
 
     public User() {
@@ -44,21 +45,22 @@ public class User {
         lastName  = "";
     }
 
-    public User(String login, String password, Roles role) {
+    public User(String login, String password, Set<Role> roles) {
         this();
         this.login = login;
         this.password = password;
-        this.role = role;
+        this.roles = roles;
     }
 
-    public User(String login, String password, Roles role, String firstName, String lastName, String email) {
+    public User(String login, String password, Set<Role> roles, String firstName, String lastName, String email) {
         this.login = login;
         this.password = password;
-        this.role = role;
+        this.roles = roles;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
+
 
     public String getLogin() {
         return login;
@@ -74,14 +76,6 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public Roles getRole() {
-        return role;
-    }
-
-    public void setRole(Roles role) {
-        this.role = role;
     }
 
     public String getFirstName() {
@@ -106,6 +100,41 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getWhiteListIP() {
+        return whiteListIP;
+    }
+
+    public void setWhiteListIP(String whiteListIP) {
+        this.whiteListIP = whiteListIP;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (!getLogin().equals(user.getLogin())) return false;
+        return getEmail().equals(user.getEmail());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getLogin().hashCode();
+        result = 31 * result + getEmail().hashCode();
+        return result;
     }
 
     @Override
